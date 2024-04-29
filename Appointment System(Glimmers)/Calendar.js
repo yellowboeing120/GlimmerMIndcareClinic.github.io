cd6c4bf7e8c19e9815f8fc01c21817302debb252
yellowboeing120 <110837558+yellowboeing120@users.noreply.github.com>
2024-04-30 02:39:02 +0800

    Delete Appointment System(Glimmers) directory

--- Appointment System(Glimmers)/Calendar.js
+++
@@ -1,7 +0,0 @@
-const times = document.querySelectorAll('.time');
-
-times.forEach(time => {
-    time.addEventListener('click', () => {
-        alert(`Appointment scheduled for ${time.getAttribute('data-time')}`);
-    });
-});
